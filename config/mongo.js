d07b91dc2652364254bad883b9fdeef0ee599d5b
abhilash <abhilash2012003@gmail.com>
2024-01-03 20:07:02 +0530

    test hosting 1

--- config/mongo.js
+++ config/mongo.js
@@ -4,9 +4,14 @@ require('dotenv').config();
 const asyncHandler = require("express-async-handler");
 
 
+const uri = 'your-mongodb-atlas-connection-string';
+
+
 const dbConnect = asyncHandler(async () => {
     try {
-        mongoose.connect(process.env.MONGO_URL);
+        // mongoose.connect(process.env.MONGO_URL);
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+
         console.log("Database Connected Successfully");
     } catch (error) {
         throw new Error();
